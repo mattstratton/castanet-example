ab9fe1df8afefbecc4194393c66fded828a02704
Matt Stratton <matt.stratton@gmail.com>
2017-05-05 09:48:41 -0500

    Add multiple sites

---
+++ bin/netlify.sh
@@ -0,0 +1,4 @@
+hugo_0.19 --theme=castanet --buildDrafts=false -d public/blue-grid --config config-blue-grid.toml
+hugo_0.19 --theme=castanet --buildDrafts=false -d public/blue-row --config config-blue-row.toml
+hugo_0.19 --theme=castanet --buildDrafts=false -d public/orange-grid --config config-orange-grid.toml
+hugo_0.19 --theme=castanet --buildDrafts=false -d public/orange-row --config config-orange-row.toml
