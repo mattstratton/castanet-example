df40ed18ae1b1f54ee5456bdf351ac3768c28c5c
Matt Stratton <matt.stratton@gmail.com>
2017-05-18 11:11:37 -0400

    Update with grey theme

--- bin/netlify.sh
+++ bin/netlify.sh
@@ -1,5 +1,7 @@
-hugo_0.19 --theme=castanet --buildDrafts=false -d public/blue-grid --config config-blue-grid.toml
-hugo_0.19 --theme=castanet --buildDrafts=false -d public/blue-row --config config-blue-row.toml
-hugo_0.19 --theme=castanet --buildDrafts=false -d public/orange-grid --config config-orange-grid.toml
-hugo_0.19 --theme=castanet --buildDrafts=false -d public/orange-row --config config-orange-row.toml
+hugo --theme=castanet --buildDrafts=false -d public/blue-grid --config config-blue-grid.toml
+hugo --theme=castanet --buildDrafts=false -d public/blue-row --config config-blue-row.toml
+hugo --theme=castanet --buildDrafts=false -d public/orange-grid --config config-orange-grid.toml
+hugo --theme=castanet --buildDrafts=false -d public/orange-row --config config-orange-row.toml
+hugo --theme=castanet --buildDrafts=false -d public/grey-grid --config config-grey-grid.toml
+hugo --theme=castanet --buildDrafts=false -d public/grey-row --config config-grey-row.toml
 cp -r home/* public/
